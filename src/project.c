96030013f98091aaa1f3d7d0229a03363d5b64af
Adam Dunson <adam.dunson@knights.ucf.edu>
2011-11-14 13:06:59 -0500

    Stepping through the program and debuggin'
    Tried running it and noticed that it wasn't even able to run the first
    instruction, so I went ahead and started debugging from there.
    
    Added lots of debug messages (switched on/off with the #define at the top). The
    messages are useful when debugging a specific function, but they could probably
    be more informative (as it is, it looks like they just give you random bits of
    information).
    
    The first couple of instructions appear to work. addi definitely works
    perfectly. Something is wrong with Jump, but we can look into that more later.

--- src/project.c
+++ src/project.c
@@ -1,5 +1,7 @@
 #include "spimcore.h"
 
+#define DEBUG_PROJECT 1
+
 // TODO:
 // figure out the rest of instruction_decode()'s control signals
 
@@ -7,9 +9,11 @@
 /* 10 Points */
 void ALU(unsigned A, unsigned B, char ALUControl, unsigned *ALUresult, char *Zero)
 {
-	//operands are passed in as unsigned values.
-	//we will need these for operation on signed
-	//integers
+	if(DEBUG_PROJECT) printf("DEBUG: A = %d, B = %d\n", A, B);
+
+	// operands are passed in as unsigned values.
+	// we will need these for operation on signed
+	// integers
 	int signedA = (int)A;
 	int signedB = (int)B;
 
@@ -17,10 +21,12 @@ void ALU(unsigned A, unsigned B, char ALUControl, unsigned *ALUresult, char *Zer
 	// this is where we calculate the result of an ALU operation (based on the ALUControl)
 	switch(ALUControl) {
 		case 0:
-			*ALUresult = signedA + signedB;
+			//*ALUresult = signedA + signedB;
+			*ALUresult = A + B;
 			break;
 		case 1:
-			*ALUresult = signedA - signedB;
+			//*ALUresult = signedA - signedB;
+			*ALUresult = A - B;
 			break;
 		case 2:
 			*ALUresult = signedA < signedB;
@@ -44,7 +50,9 @@ void ALU(unsigned A, unsigned B, char ALUControl, unsigned *ALUresult, char *Zer
 		default:
 			printf("Invalid control value %d passed to the ALU.\n", ALUControl);
 	}
-	*Zero == !(*ALUresult);
+	*Zero = !(*ALUresult);
+
+	if(DEBUG_PROJECT) printf("DEBUG: ALUControl = %d, ALUresult = %d, Zero = %d\n", ALUControl, *ALUresult, *Zero);
 }
 
 /* instruction fetch */
@@ -54,13 +62,16 @@ int instruction_fetch(unsigned PC, unsigned *Mem, unsigned *instruction)
 	//TODO: check appropriate return value for errors (alignment and
 	//out of bounds are considered two different errors)
 
+	if(DEBUG_PROJECT) printf("DEBUG: wordalign = %d, pc = 0x%X, (pc >> 2) = 0x%X, memsize = 0x%X\n", !(PC % 4), PC, PC >> 2, MEMSIZE);
+
 	// this should (ideally) check for word alignment and check for out-of-bounds
-	if((PC >> 2) % 4 != 0 || (PC >> 2) >= MEMSIZE) {
+	if(PC % 4 != 0 || (PC >> 2) >= MEMSIZE) {
+		if(DEBUG_PROJECT) printf("DEBUG: HALT!\n");
 		return 1;
 	}
 
 	// Mem is an array of words and PC is the actual address value
-	*instruction = Mem[PC];
+	*instruction = Mem[PC >> 2];
 
 	return 0;
 }
@@ -68,7 +79,7 @@ int instruction_fetch(unsigned PC, unsigned *Mem, unsigned *instruction)
 
 /* instruction partition */
 /* 10 Points */
-void instruction_partition(unsigned instruction, unsigned *op, unsigned *r1,unsigned *r2, unsigned *r3, unsigned *funct, unsigned *offset, unsigned *jsec)
+void instruction_partition(unsigned instruction, unsigned *op, unsigned *r1, unsigned *r2, unsigned *r3, unsigned *funct, unsigned *offset, unsigned *jsec)
 {
 	// For the following, I use an alternating (1010...) bit string to represent
 	// the value that we are trying to isolate. X's represent "don't care"s.
@@ -123,6 +134,8 @@ void instruction_partition(unsigned instruction, unsigned *op, unsigned *r1,unsi
 	// 0000 0011 1111 1111 1111 1111 1111 1111 =
 	// 0000 0010 1010 1010 1010 1010 1010 1010
 	*jsec = instruction & 0x03FFFFFF;
+
+	if(DEBUG_PROJECT) printf("DEBUG: op = 0x%X, r1 = 0x%X, r2 = 0x%X, r3 = 0x%X, funct = 0x%X, offset = 0x%X, jsec = 0x%X\n", *op, *r1, *r2, *r3, *funct, *offset, *jsec);
 }
 
 
@@ -134,23 +147,27 @@ int instruction_decode(unsigned op, struct_controls *controls)
 	// also check the logic involved as I am not entirely confident that they are
 	// correct.
 
-	// set up some nice "don't care" values for controls
+	// set up some nice default values for controls
 	controls->MemRead = 2;
 	controls->MemWrite = 2;
 	controls->RegWrite = 2;
 
 	controls->RegDst = 2;
-	controls->Jump = 2;
 	controls->Branch = 2;
-	controls->MemtoReg = 2;
 
 	controls->ALUSrc = 2;
 	controls->ALUOp = 0;
+
+	// these NEED to be 0, otherwise some stuff gets messed up in PC_update,
+	// write_register, etc.
+	controls->MemtoReg = 0;
+	controls->Jump = 0;
 
 	switch(op) {
 		case 0x00:
 			/* R-type instructions */
 			// See FAQ, Q8 regarding what do with ALUOp for R-type instructions
+			controls->RegWrite = 1;
 			controls->RegDst = 1;
 			controls->ALUSrc = 0;
 			controls->ALUOp = 7;
@@ -161,9 +178,12 @@ int instruction_decode(unsigned op, struct_controls *controls)
 		default:
 			/* I-type instructions */
 			controls->ALUSrc = 1;
+			controls->RegDst = 0;
+			controls->RegWrite = 1;
 			switch(op) {
 				case 0x04: // beq
 					controls->Branch = 1;
+					controls->RegWrite = 0;
 					break;
 				case 0x08: // addi
 					break;
@@ -174,16 +194,18 @@ int instruction_decode(unsigned op, struct_controls *controls)
 					controls->ALUOp = 3;
 					break;
 				case 0x0F: // lui
+					controls->ALUOp = 6;
 					break;
 				case 0x23: // lw
 					controls->MemRead = 1;
-					controls->RegWrite = 1;
+					controls->MemtoReg = 1;
 					break;
 				case 0x2B: // sw
 					controls->RegDst = 2;
 					controls->MemWrite = 1;
-					controls->MemtoReg = 1;
+					controls->RegWrite = 0;
 				default: // invalid instruction
+					if(DEBUG_PROJECT) printf("DEBUG: HALT!\n");
 					return 1;
 			}
 	}
@@ -212,22 +234,29 @@ void read_register(unsigned r1, unsigned r2, unsigned *Reg, unsigned *data1, uns
 /* 10 Points */
 void sign_extend(unsigned offset, unsigned *extended_value)
 {
-	//offset is a 16-bit signed value
-	unsigned upperBytes = 0xFFFF0000;
-	if(*extended_value >> 15)
-		*extended_value |= upperBytes;
+	// AND out the upper bits and store the lower bits of offset into
+	// extended_value
+	*extended_value = (offset & 0x0000FFFF);
+
+	// check if bit 15 is set (ie, "is this negative?")
+	// if so, set the upper 16 bits to 1
+	if(*extended_value & 0x00008000)
+		*extended_value += 0xFFFF0000;
 }
 
 
 /* ALU operations */
 /* 10 Points */
-// TODO: Need to account for ALUControl = 6 and ALUControl = 7 below
+// TODO: Need to account for ALUControl = 7 below
 int ALU_operations(unsigned data1, unsigned data2, unsigned extended_value, unsigned funct, char ALUOp, char ALUSrc, unsigned *ALUresult, char *Zero)
 {
 	// ALU is not called from core. It looks like we will need to call it here. 
 
 	// invalid op is anything outside the range [0, 7]
-	if(ALUOp < 0 || ALUOp > 7) return 1;
+	if(ALUOp < 0 || ALUOp > 7) {
+		if(DEBUG_PROJECT) printf("DEBUG: HALT!\n");
+		return 1;
+	}
 
 	// check for no-op conditions
 	if(ALUOp == 7 && funct == 0) return 0;
@@ -235,8 +264,6 @@ int ALU_operations(unsigned data1, unsigned data2, unsigned extended_value, unsi
 	// ALUControl is passed to ALU(...) and determines what ALU operation to do
 	// See item #4 (page 3) of the Final Project PDF for the possible values
 	char ALUControl = ALUOp;
-
-	if(ALUSrc) data2 = extended_value;
 
 	// ALUOp == 7 essentially means "use the function code"
 	if(ALUOp == 7) {
@@ -261,11 +288,12 @@ int ALU_operations(unsigned data1, unsigned data2, unsigned extended_value, unsi
 				ALUControl = 5;
 				break;
 			default: // invalid function
+				if(DEBUG_PROJECT) printf("DEBUG: HALT!\n");
 				return 1;
 		}
 	}
 
-	ALU(data1, data2, ALUControl, ALUresult, Zero);
+	ALU(data1, (ALUSrc ? extended_value : data2), ALUControl, ALUresult, Zero);
 	return 0;
 }
 
@@ -275,7 +303,10 @@ int ALU_operations(unsigned data1, unsigned data2, unsigned extended_value, unsi
 int rw_memory(unsigned ALUresult, unsigned data2, char MemWrite, char MemRead, unsigned *memdata, unsigned *Mem)
 {
 	// check for memory out of bounds
-	if((ALUresult >> 2) >= MEMSIZE) return 1;
+	if((ALUresult >> 2) >= MEMSIZE) {
+		if(DEBUG_PROJECT) printf("DEBUG: HALT!\n");
+		return 1;
+	}
 
 	//read or write if MemRead/MemWrite are nonzero
 	if(MemRead) {
@@ -293,12 +324,19 @@ int rw_memory(unsigned ALUresult, unsigned data2, char MemWrite, char MemRead, u
 /* 10 Points */
 void write_register(unsigned r2, unsigned r3, unsigned memdata, unsigned ALUresult, char RegWrite, char RegDst, char MemtoReg, unsigned *Reg)
 {
+	// TODO: Need to check what happens if we get passed a no-op with non-zero
+	// r2, or r3 ... I am guessing that it will (for right now) just overwrite
+	// those registers with 0 (and that's bad).
+	//
+	//
 	// ASSUMPTION: RegWrite is the control signal that determines whether a 
 	// reg write will happen
 	if(RegWrite) {
-		unsigned write_data = (MemtoReg ? memdata : ALUresult);
+		unsigned write_data = (MemtoReg == 1 ? memdata : ALUresult);
 
-		if(RegDst)
+		if(DEBUG_PROJECT) printf("DEBUG: RegDst = %d, write_data = %d, r2 = %d, r3 = %d\n", RegDst, write_data, r2, r3);
+
+		if(!RegDst)
 			Reg[r2] = (r2 ? write_data : 0);
 		else
 			Reg[r3] = (r3 ? write_data : 0);
@@ -310,7 +348,9 @@ void write_register(unsigned r2, unsigned r3, unsigned memdata, unsigned ALUresu
 /* 10 Points */
 void PC_update(unsigned jsec, unsigned extended_value, char Branch, char Jump, char Zero, unsigned *PC)
 {
+	if(DEBUG_PROJECT) printf("DEBUG: PC before update = 0x%X\n", *PC);
 	*PC += 4;
+
 	if(Branch && Zero) *PC += (extended_value << 2);
 
 	// extended_value is the branch offset
@@ -322,5 +362,6 @@ void PC_update(unsigned jsec, unsigned extended_value, char Branch, char Jump, c
 
 		*PC |= (jsec<<2);
 	}
+	if(DEBUG_PROJECT) printf("DEBUG: PC after update = 0x%X\n", *PC);
 }
 
